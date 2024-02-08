a4f28988f83647b3cd14dd4ff4efecef937a696d
ayan4m1 <andrew@bulletlogic.com>
2024-02-08 17:14:15 -0500

    rewrite entire lib, remove through2 and make it ESM

--- index.js
+++ index.js
@@ -1,62 +1,55 @@
-const fs = require('fs');
+import stylelint from 'stylelint';
+import { Transform } from 'readable-stream';
+import { readFile, writeFile } from 'fs/promises';
 
-const stylelint = require('stylelint');
-const through = require('through2');
+const { lint } = stylelint;
 
-/**
- * This should not be as complicated as it is.
- * Because stylelint does not seem to return a "fixed" CSS string, we
- * have to play games with the filesystem aimed at getting this to work
- * in a Yeoman transform stream.
- */
-module.exports = function(options) {
-  return through.obj(function(file, encoding, callback) {
-    const lintOptions = Object.assign({}, options, {
-      code: file.contents.toString(encoding),
-      codeFilename: file.path
-    });
+export default function (options) {
+  return new Transform({
+    objectMode: true,
+    async transform(file, encoding, callback) {
+      const lintOptions = {
+        ...options,
+        code: file.contents.toString(encoding),
+        codeFilename: file.path
+      };
 
-    try {
-      // first, check to see if the file needs linting
-      stylelint.lint(lintOptions).then(function(result) {
+      try {
+        const result = await lint(lintOptions);
         const lintResult = result.results.shift();
 
         if (lintResult && lintResult.errored) {
           // we need to write the file to disk before Yeoman does
           // so that stylelint can run in fix mode
-          fs.writeFile(file.path, file.contents, err => {
+          await writeFile(file.path, file.contents);
+
+          // now we are asking stylelint to fix the file we just wrote
+          const fixOptions = Object.assign({}, options, {
+            files: file.path,
+            syntax: 'scss',
+            fix: true
+          });
+
+          await lint(fixOptions);
+
+          // the fix is applied in-place, so Yeoman will overwrite
+          // it with the incorrect version - so we have to read it
+          // back and update the contents passed to Yeoman
+          readFile(file.path, (err, data) => {
             if (err) {
               return callback(err);
             }
 
-            // now we are asking stylelint to fix the file we just wrote
-            const fixOptions = Object.assign({}, options, {
-              files: file.path,
-              syntax: 'scss',
-              fix: true
-            });
-
-            stylelint.lint(fixOptions).then(function() {
-              // the fix is applied in-place, so Yeoman will overwrite
-              // it with the incorrect version - so we have to read it
-              // back and update the contents passed to Yeoman
-              fs.readFile(file.path, (err, data) => {
-                if (err) {
-                  return callback(err);
-                }
-
-                file.contents = data;
-                callback(null, file);
-              });
-            });
+            file.contents = data;
+            callback(null, file);
           });
         } else {
           // no need to lint the file
           callback(null, file);
         }
-      });
-    } catch (error) {
-      callback(error);
+      } catch (error) {
+        callback(error);
+      }
     }
   });
-};
+}
